e87d3c753570c52f3cb1906001a0f6b2be6183d0
Stivencl <stevfoun@gmail.com>
2024-10-24 20:30:21 -0500

    crud terminado

--- api.js
+++ api.js
@@ -60,4 +60,43 @@ router.post('/create-task', function (req, res) {
     });
 });
 
+//search task
+
+router.get('/all-tasks', function (req, res) {
+    TaskModel.find(function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+//update task
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+//Delete task
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
 module.exports = router;
